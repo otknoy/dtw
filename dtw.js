e6935e16e5bb7ec32e94cb37bdce735956b0b3a5
otknoy <otknoy@gmail.com>
2015-11-18 02:46:24 +0900

    Disable window size

--- dtw.js
+++ dtw.js
@@ -11,23 +11,32 @@ function createMatrix(n, m, v) {
     return d;
 }
 
-DTW.distance = function(ts1, ts2, distFunc) {
+DTW.distance = function(ts1, ts2, distFunc, w) {
     var n = ts1.length;
     var m = ts2.length;
+
+    // default window size
+    if (typeof w === 'undefined') {
+	w = Math.max(n, m);
+    }
+
+    w = Math.max(w, Math.abs(n - m));
+    // console.log(w);
 
     var costMatrix = createMatrix(n+1, m+1, Number.POSITIVE_INFINITY);
     costMatrix[0][0] = 0;
 
     for (var i = 1; i <= n; i++) {
-	for (var j = 1; j <= n; j++) {
+	var begin = Math.max(1, i - w);
+	var end   = Math.min(m, i + w);
+	for (var j = begin; j <= end; j++) {
 	    var cost = distFunc(ts1[i-1], ts2[j-1]);
 	    costMatrix[i][j] = cost + Math.min(costMatrix[i-1][j  ],
 					       costMatrix[i  ][j-1],
 					       costMatrix[i-1][j-1]);
 	}
     }
-
-    console.log(costMatrix);
+    // console.log(costMatrix);
 
     return costMatrix[n][m];
 };
